656dc3a71c412fa794a53bf0d3f7f54b11b7efde
jacob1 <jfu614@gmail.com>
2012-10-01 22:00:57 -0400

    Stamp fix button to bring back deleted stamps
    (and stamps deleted before version 74.2)

--- src/Config.h
+++ src/Config.h
@@ -21,11 +21,11 @@
 #endif
 
 #ifndef MINOR_VERSION
-#define MINOR_VERSION 5
+#define MINOR_VERSION 9
 #endif
 
 #ifndef BUILD_NUM
-#define BUILD_NUM 214
+#define BUILD_NUM 232
 #endif
 
 #ifndef SNAPSHOT_ID
